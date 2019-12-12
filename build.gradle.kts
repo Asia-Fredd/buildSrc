f945719559346e717ffa1d62630db1ac0448ee5c
FreddY <55474596+Asia-Fredd@users.noreply.github.com>
2019-12-12 17:59:42 +0800

    建立 build.gradle.kts, 引入 kotlin-dsl, 指定 source 位置.
    Signed-off-by: FreddY <55474596+Asia-Fredd@users.noreply.github.com>

---
+++ build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    `kotlin-dsl`
+}
+repositories {
+    // The org.jetbrains.kotlin.jvm plugin requires a repository
+    // where to download the Kotlin compiler dependencies from.
+    // Required since Gradle 4.10+.
+    // https://proandroiddev.com/gradle-dependency-management-with-kotlin-94eed4df9a28#a296
+    jcenter()
+}
+sourceSets["main"].withConvention(org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet::class) {
+    kotlin.srcDir("kotlin")
+}
